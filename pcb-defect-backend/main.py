38136a72b57c57c71f085058ef0245be25abdf91
Prashant Yadav <prashanttyadav12@gmail.com>
2026-06-16 23:44:31 +0530

    Update main.py

--- pcb-defect-backend/main.py
+++ pcb-defect-backend/main.py
@@ -12,6 +12,14 @@ app = FastAPI(
     description="Batch PCB Defect Detection API",
     version="1.0.0"
 )
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # ================= MODEL LOAD =================
 MODEL_PATH = "best.pt"   # adjust if needed
